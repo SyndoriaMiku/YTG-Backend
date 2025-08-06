1bfca698f125927fb1c49dbc883eeb49927c6325
SyndoriaMiku <61247021+SyndoriaMiku@users.noreply.github.com>
2025-08-06 09:34:19 +0700

    wip login and setting

--- YTG/Backend/views.py
+++ YTG/Backend/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
+
+
+class LoginAPIView(APIView):
+    """
+    API view for user login.
+    """
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            is_admin = user.is_staff or user.is_superuser
+
+            return Response({
+                'access': str(refresh.access_token),
+                'refresh': str(refresh),
+                'is_admin': is_admin,
+                'username': user.username,
+            })
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
--- YTG/YTG/settings.py
+++ YTG/YTG/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=$8d-ool7w^0w1zhv38uc4da%7=9kw@2=sqc2b@c3-fa*n5hc-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,7 +45,8 @@ INSTALLED_APPS = [
     
     #Third party
     'rest_framework',
-    'corheaders',
+    'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
