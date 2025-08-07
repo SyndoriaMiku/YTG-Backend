a4ad81d0e80985608501abe7c2fe1270135f86cd
SyndoriaMiku <61247021+SyndoriaMiku@users.noreply.github.com>
2025-08-07 10:00:22 +0700

    complete update

--- YTG/Backend/views.py
+++ YTG/Backend/views.py
@@ -6,6 +6,8 @@ from rest_framework import status
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.utils.translation import gettext as _
+from django.db import IntegrityError
+from django.utils import timezone
 
 from . import serializers
 from . import models
@@ -59,13 +61,56 @@ class LogoutAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        try:
-            refresh_token = request.data.get('refresh')
-            if not refresh_token:
-                return Response({'message': _('Refresh token is required')}, status=status.HTTP_400_BAD_REQUEST)
+        refresh_token = request.data.get('refresh')
 
-            # Blacklist the token
-            models.BlacklistedToken.objects.create(token=refresh_token)
-            return Response({'message': _('Logout successful')}, status=status.HTTP_200_OK)
+        if not refresh_token:
+            return Response({'message': _('Refresh token is required')}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({'message': _('Logout successful')}, status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
-            return Response({'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': _('Invalid token')}, status=status.HTTP_400_BAD_REQUEST)
+        
+class UpdateUserAPIView(APIView):
+    """
+    API view for updating user profile.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def patch(self , request):
+        user = request.user
+        data = request.data
+
+        updated_fields = []
+
+        #update data
+        if 'email' in data:
+            user.email = data['email']
+            updated_fields.append('email')
+
+        if 'phone' in data:
+            user.phone = data['phone']
+            updated_fields.append('phone') 
+        
+        if 'nickname' in data:
+            new_nickname = data['nickname']
+            if user.nickname != new_nickname:
+                if user.check_name_change_limit():
+                    user.nickname = new_nickname
+                    user.last_name_change = timezone.now()
+                    updated_fields.extend('nickname', 'last_name_change')
+                else:
+                    return Response({'message': _('You can only change your nickname once every 30 days.')}, status=status.HTTP_400_BAD_REQUEST)
+                
+        #Update if changes were made
+            try:
+                user.save(update_fields=updated_fields)
+                return Response({'message': _('User profile updated successfully')}, status=status.HTTP_200_OK)
+            except IntegrityError as e:
+                return Response({'message': _('Error updating profile')}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'message': _('No changes made to the profile')}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+            
