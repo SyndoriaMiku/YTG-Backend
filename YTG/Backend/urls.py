bd124ece684ad758798e7389ecc4ff786ae2989e
SyndoriaMiku <61247021+SyndoriaMiku@users.noreply.github.com>
2025-08-08 08:49:42 +0700

    url for add point and tournament

--- YTG/Backend/urls.py
+++ YTG/Backend/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('register/', views.RegisterAPIView.as_view(), name='register'),
     path('login/', views.LoginAPIView.as_view(), name='login'),
     path('logout/', views.LogoutAPIView.as_view(), name='logout'),
+    path('point/add/', views.PointAddAPIView.as_view(), name='point_add'),
+    path('tournament/add', views.TournamentAddAPIView.as_view(), name='tournament_add'),
 ]
